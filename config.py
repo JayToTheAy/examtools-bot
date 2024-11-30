e983ec132e3e11b5019a9a7d4ad738a4f00fdbb8
Jacob Humble <110194066+JayToTheAy@users.noreply.github.com>
2024-11-30 09:33:46 -0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+"""Holds our config"""
+VERSION = "1.0.0"
+TOKEN = "[your token here]"
+USER_AGENT = "ExamHelper/[your callsign]/" + VERSION
+OWNER_ID = "[owner account id]"
+MY_GUILD_ID = "[test guild id]"
